7ff57cdcfc111ade4dc71eaa6e6ce63f50f4b7b9
Liu-Wen-Yuan <15816141883@163.com>
2017-09-22 04:15:19 -0500

    Update kernel.c

--- kernel.c
+++ kernel.c
@@ -3,19 +3,19 @@
 */
 void kmain(void)
 {
-	const char *str = "Hello world !";
+	const char *str = "Hello world! ";
 	char *vidptr = (char*)0xb8000; 	//video mem begins here.
 	unsigned int i = 0;
 	unsigned int j = 0;
 
 	/* this loops clears the screen
-	* there are 25 lines each of 80 columns; each element takes 2 bytes */
-	while(j < 80 * 25 * 2) {
-		/* blank character */
-		vidptr[j] = ' ';
-		/* attribute-byte - light grey on black screen */
-		vidptr[j+1] = 0x07; 		
-		j = j + 2;
+	* there are 25 lines each of 80 columns; each element takes 2 bytes
+	*while(j < 80 * 25 * 2) {
+	*	 blank character 
+	*	vidptr[j] = ' ';
+	*       attribute-byte - light grey on black screen
+	*	vidptr[j+1] = 0x07; 		
+	*	j = j + 2; */
 	}
 
 	j = 0;
